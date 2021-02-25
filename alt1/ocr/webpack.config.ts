c0a8bbb923adb0c05deba874e54812e4a34514bf
skillbert <wilbert.terhaar@gmail.com>
2021-02-25 17:32:58 +0100

    remove bundle from ocr font names

--- alt1/ocr/webpack.config.ts
+++ alt1/ocr/webpack.config.ts
@@ -3,25 +3,26 @@ import * as fs from "fs";
 import * as path from "path";
 import { chainAlt1Lib, addAlt1Externals } from "../../scripts/alt1webpack";
 
+//standard build steps
 var cnf = chainAlt1Lib(__dirname);
 
 var configs = [cnf];
+//extra builds for fonts
 
 var fontdir = path.resolve(__dirname, "fonts");
 var files = fs.readdirSync(fontdir);
 for (var file of files) {
 	var m = file.match(/([\w-]+)\.fontmeta\.json$/i);
 	if (m) {
-		var name="fonts/" + m[1];
+		var fontname = m[1];
 		var fontcnf = new A1webpack(__dirname);
-		fontcnf.chain.output.path(path.resolve(__dirname, "./dist"));
-		fontcnf.makeUmd(m[1], m[1]);
-		fontcnf.entry(name,path.resolve(fontdir,file));
-		//addAlt1Externals(fontcnf);
+		fontcnf.chain.output.path(path.resolve(__dirname, "./dist/fonts"));
+		fontcnf.chain.output.filename("[name].js");
+		fontcnf.makeUmd(fontname, "OCR_" + fontname);
+		fontcnf.entry(fontname, path.resolve(fontdir, file));
 		configs.push(fontcnf);
 	}
 }
-
 
 configs.forEach(c => c.chain.resolveLoader.modules.add(path.resolve(__dirname, "../../node_modules")).add(path.resolve(__dirname, "../")));
 var rawcnf = configs.map(c => c.toConfig());
