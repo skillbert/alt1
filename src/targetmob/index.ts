3ae92e2a1ed9d2ce745089706e531f0cf95b58a4
Ex Inferi <ex.inferi.rs3@gmail.com>
2024-12-14 17:05:43 +0100

    Targetmob fix (#35)
    * Fix hp, because Jagex broke it by adding a comma
    
    * Add legacy support
    
    * Reduce the amount `findSubimage()` is called
    
    * Use regex for replace comma

--- src/targetmob/index.ts
+++ src/targetmob/index.ts
@@ -5,17 +5,23 @@ import { webpackImages, ImgRef } from "alt1/base";
 var chatfont = require("../fonts/aa_8px.fontmeta.json");
 
 var imgs = webpackImages({
-	detectimg: require("./imgs/detectimg.data.png")
+	detectimg: require("./imgs/detectimg.data.png"),
+	detectleg: require("./imgs/detectimgLegacy.data.png")
 });
 
 export default class TargetMobReader {
 
 	state: { hp: number, name: string } | null = null;
 	lastpos: a1lib.PointLike | null = null;
+	legacy: boolean = false;
 
 	read(img?: ImgRef) {
 		if (!img) { img = a1lib.captureHoldFullRs(); }
-		var pos = img.findSubimage(imgs.detectimg);
+		if (!this.lastpos) {
+			var leg = img.findSubimage(imgs.detectleg);
+			if (leg.length != 0) { this.legacy = true; } 
+		}
+		var pos = this.legacy ?	img.findSubimage(imgs.detectleg) : img.findSubimage(imgs.detectimg);
 		if (pos.length != 0) {
 			var data = img.toData(pos[0].x - 151, pos[0].y - 16, 220, 44);
 			var mobname = OCR.findReadLine(data, chatfont, [[255, 255, 255]], 62, 18, 20, 1);
@@ -23,7 +29,7 @@ export default class TargetMobReader {
 			this.lastpos = pos[0];
 			this.state = {
 				name: mobname.text,
-				hp: +mobhp.text
+				hp: +mobhp.text.replace(/,/g, "")
 			};
 		} else {
 			this.state = null;
